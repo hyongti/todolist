cdb34439ba6056621548b3d8c7dc5efcbfa1d211
hyongti <hyongtiii@gmail.com>
2021-11-22 12:14:11 +0900

    TodoTemplate 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
+import TodoTemplate from "./components/TodoTemplate";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <div>안녕하세요</div>
+      <TodoTemplate>안녕하세요</TodoTemplate>
     </>
   );
 }
